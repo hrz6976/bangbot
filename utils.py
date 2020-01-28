fa76c6d8e53d1f5edc1e56889e0dbc19d092d1fb
12f23eddde <hrz6976@hotmail.com>
2020-01-28 23:13:31 +0800

    用sed替换awk
    awk太神必了

--- utils.py
+++ utils.py
@@ -3,13 +3,14 @@ import subprocess
 
 #   return touch_screen_event(str)
 #   ref:https://stackoverflow.com/questions/54228228/adb-how-to-programmatically-determine-which-input-device-is-used-for-sending-to
-#   Modified: return bottom line, remove '\n'
-#   Issues found with Samsung S10 (API29): Using string as RS in awk: only first char takes effect
+#   some older devices does not implement awk
+#   execute in shell: compatibility
+#   f*** awk
 def find_touchscreen():
-    cmd_string = 'adb shell ' + "getevent -pl | grep -B 100 ABS_MT_POSITION_X | \
-    awk '/add device/ {print $NF}' | awk 'END {print}'"
-    out_bytes = subprocess.check_output(cmd_string.split())
-    return out_bytes.decode('utf-8')[:-1]  # remove '\n'
+    cmd_string = 'adb exec-out ' + 'getevent -lp | grep -B 100 ABS_MT_POSITION_X | \
+    grep "/dev/input/event" | sed -e "s/^add device [0-9]\{1,2\}: //" | tail -n 1'
+    res_string = subprocess.check_output(cmd_string, shell=True).decode('utf-8')[:-1]
+    return res_string
 
 
 #   return device api version(int)
@@ -30,16 +31,17 @@ def execute_command(cmd_string, print_to_stdout=False):
 
 #   local_path, remote_path include filename
 #   force=False: check if file exists before push
+#   execute in shell: compatibility
 def push(local_path, remote_path, force=False):
     if force is False:
         cmd_string = 'adb shell [ -e ' + remote_path + ' ] && echo "Found" || echo "NotFound"'
         out_bytes = subprocess.check_output(cmd_string.split())
         if out_bytes.decode('utf-8') is not 'Found\n':
             cmd_string = 'adb push ' + local_path + ' ' + remote_path
-            subprocess.check_output(cmd_string.split())
+            subprocess.check_output(cmd_string, shell=True)
     else:
         cmd_string = 'adb push ' + local_path + ' ' + remote_path
-        subprocess.check_output(cmd_string.split())
+        subprocess.check_output(cmd_string, shell=True)
 
 
 if __name__ == '__main__':
